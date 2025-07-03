17827a4049e83414de5a7bccfe8318c1f7c28ff3
YetAnotherNotHacking <yetanotherdingus@gmail.com>
2025-07-03 06:14:52 +0000

    remove old content since this is not implemented yet.

--- services/email.py
+++ services/email.py
@@ -1,12 +1,1 @@
-import os
-import dotenv
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from flask_mail import Mail, Message
-
-dotenv.load_dotenv()
-
-app = Flask(__name__)
-CORS(app)
-
-mail = Mail(app)
+# not implemented
